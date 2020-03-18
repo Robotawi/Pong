69445109989ee6c9be28ac7a956e24f74516d104
Robotawi <mohamed.s.raessa@ieee.org>
2020-03-18 21:24:20 +0900

    Implement ball class

--- src/ball.cpp
+++ src/ball.cpp
@@ -4,48 +4,78 @@
 
 #include "ball.h"
 #include <iostream>
+#include <chrono>
+#include <thread>
 using namespace std;
 
-Ball::Ball(int & screen_width, int & screen_height) {
+Ball::Ball(int &screen_width, int &screen_height, int &grid_width, int &grid_height) {
+    radius = screen_height/grid_height;
+
     init_x = screen_width/2;
     init_y = screen_height/2;
     curr_x = init_x;
     curr_y = init_y;
+
+    speed = 2;
 }
 void Ball::reset() {
     curr_x = init_x;
     curr_y = init_y;
+    this->randDirect();
 }
 
 void Ball::move() {
     switch(direction){
         case STOP:
             break;
-        case LEFT:
-            curr_x--;
-            break;
         case ULEFT:
-            curr_x--;
-            curr_y--;
+            curr_x-=speed;
+            curr_y-=speed;
             break;
         case DLEFT:
-            curr_x--;
-            curr_y++;
-            break;
-        case RIGHT:
-            curr_x++;
+            curr_x-=speed;
+            curr_y+=speed;
             break;
         case URIGHT:
-            curr_x++;
-            curr_y--;
+            curr_x+=speed;
+            curr_y-=speed;
             break;
         case DRIGHT:
-            curr_x++;
-            curr_y++;
+            curr_x+=speed;
+            curr_y+=speed;
             break;
     }
 }
-
+/*
+void Ball::move() {
+    switch(direction){
+        case STOP:
+            break;
+        case LEFT:
+            curr_x-=speed;
+            break;
+        case ULEFT:
+            curr_x-=speed;
+            curr_y-=speed;
+            break;
+        case DLEFT:
+            curr_x-=speed;
+            curr_y+=speed;
+            break;
+        case RIGHT:
+            curr_x+=speed;
+            break;
+        case URIGHT:
+            curr_x+=speed;
+            curr_y-=speed;
+            break;
+        case DRIGHT:
+            curr_x+=speed;
+            curr_y+=speed;
+            break;
+    }
+}
+*/
 int Ball::getX() {
     return curr_x;
 }
@@ -56,7 +86,8 @@ int Ball::getY() {
 
 void Ball::randDirect() {
     std::mt19937 genseed(rd());
-    std::uniform_int_distribution<int> dist(1, 6);
+    std::uniform_int_distribution<int> dist(1, 4);
+
     direction = (Bdirec)dist(genseed);
 }
 // for trial
@@ -65,18 +96,23 @@ int Ball::getDirection(){
     return direction;
 }
 
-
-int main(){
-    int width = 300;
-    int height = 200;
-
-    Ball myball(width, height);
-    myball.randDirect();
-    int newdirec = myball.getDirection();
-    cout << newdirec << endl;
-    cout << myball.getX() << ", " << myball.getY() << endl;
-    myball.move();
-    cout << myball.getX() << ", " << myball.getY() << endl;
-    myball.move();
-    cout << myball.getX() << ", " << myball.getY() << endl;
+int Ball::getRadius() {
+    return radius;
 }
+void Ball::redirect(int &&d) {
+    direction = (Bdirec)d;
+}
+//int main(){
+//    int width = 300;
+//    int height = 200;
+//
+//    Ball myball(width, height);
+//    myball.randDirect();
+//    int newdirec = myball.getDirection();
+//    cout << newdirec << endl;
+//    cout << myball.getX() << ", " << myball.getY() << endl;
+//    myball.move();
+//    cout << myball.getX() << ", " << myball.getY() << endl;
+//    myball.move();
+//    cout << myball.getX() << ", " << myball.getY() << endl;
+//}
--- src/ball.h
+++ src/ball.h
@@ -11,19 +11,25 @@
 
 class Ball:public MovingObject{
 public:
-    enum Bdirec {STOP = 0, LEFT = 1, ULEFT = 2, DLEFT = 3, RIGHT = 4, URIGHT = 5, DRIGHT = 6};
-    Ball(int & screen_width, int & screen_height);
+//    enum Bdirec {STOP = 0, LEFT = 1, ULEFT = 2, DLEFT = 3, RIGHT = 4, URIGHT = 5, DRIGHT = 6};
+    enum Bdirec {STOP = 0, ULEFT = 1, DLEFT = 2, URIGHT = 3, DRIGHT = 4};
+
+    //TODO: think about the ball radius
+    Ball(int &screen_width, int &screen_height, int &grid_width, int &grid_height);
     void reset();
     int getX();
     int getY();
     // for trial
     int getDirection();
+    int getRadius();
     void move();
-    void redirect(Bdirec d);
+    void redirect(int &&d);
     void randDirect();
 
 private:
     Bdirec direction;
     std::random_device rd;
+    int radius;
+    int speed;
 };
 #endif //SDL2TEST_BALL_H
